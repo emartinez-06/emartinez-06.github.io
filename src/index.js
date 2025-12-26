bef50b50d0158f6d309a6b4a940df311194ba2f5
Erick Martinez <erickmartinez0131@gmail.com>
2025-12-25 19:45:03 -0600

    l

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Hero from './components/Hero';
-import LaptopScene from './components/LaptopScene';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
@@ -10,7 +9,6 @@ const App = () => {
   return (
     <>
       <Hero />
-      <LaptopScene />
       <About />
       <Projects />
       <Contact />
